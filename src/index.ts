c88fb78379675de758d7a2fb058bd02cf4e3c15d
Wil Lee <kourge@gmail.com>
2016-09-16 17:03:03 -0700

    Commit initial source

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+export type AnyType = string | symbol;
+
+export interface Action<Type extends AnyType, Payload> {
+  type: Type;
+  payload: Payload;
+  error?: boolean;
+}
+
+export interface Meta<T> {
+  meta: T;
+}
+
+export interface ErrorAction<Type extends AnyType> extends Action<Type, Error> {
+  error: true;
+}
+
+export type AnyAction = Action<AnyType, any>;
+
+const validKeys = ['type', 'payload', 'error', 'meta'];
+
+function isValidKey(key: string): boolean {
+  return validKeys.indexOf(key) !== -1;
+}
+
+export function isAction<Type extends AnyType, Payload>(
+  action: any
+): action is Action<Type, Payload> {
+  return (
+    typeof action !== 'undefined' &&
+    typeof action.type !== 'undefined' &&
+    Object.keys(action).every(isValidKey)
+  );
+}
+
+export function isError<Type extends AnyType>(
+  action: Action<Type, any>
+): action is ErrorAction<Type> {
+  return action.error === true;
+}
